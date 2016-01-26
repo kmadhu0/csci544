72042aad099ca0f64bfbb711acaad76127b2ba90
Madhusudhan Krishnamachari <madhusudhankrishnamachari@gmail.com>
2016-01-25 21:30:51 -0800

    Create anagram.py
    This program uses a recursive algorithm to find out all permutations of a given input word. The program handles only strings with unique characters. For example, it takes only strings like, 'ron', 'alpine', 'water', etc. It does not work properly for strings like 'church', 'temple', where one character appears more than once. 
    
    Usage:
    
    python anagram.py <word> <output file name>
    
    E.g: python anagram.py ron output.txt

---
+++ anagram.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+from sys import argv
+results=[]
+def anagram(s,x,y):
+	if x==y:
+		results.append(''.join(s))
+	else:
+		for i in range(x,y+1):
+			s[x],s[i]=s[i],s[x]
+			anagram(s,x+1,y)
+			s[x],s[i]=s[i],s[x]
+
+s=argv[1]
+f=argv[2]
+anagram(list(s),0,len(s)-1)
+results.sort()
+file1 = open(f, 'w')
+for i in range(0,len(results)):
+	if i!=len(results)-1:
+		file1.write(results[i]+"\n")
+	else:
+		file1.write(results[i])
